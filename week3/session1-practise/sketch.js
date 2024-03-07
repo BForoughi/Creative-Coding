57aa22a3a07e803ad61177c5fa1dbf33ca514ef6
BForoughi <147166034+BForoughi@users.noreply.github.com>
2024-03-07 11:52:59 +0000

    moving my code from p5 editor to vscode

--- week3/session1-practise/sketch.js
+++ week3/session1-practise/sketch.js
@@ -1,7 +1,27 @@
-function setup() {
-  createCanvas(400, 400);
+let step
+let numAcross=50
+
+function setup(){
+  createCanvas(windowWidth, windowHeight);
+  step=width/numAcross
 }
 
-function draw() {
+function draw(){
   background(220);
+  for(let j=0; j<numAcross;j++){
+    for(let i=0; i<numAcross; i++){
+      drawTile(i,j,step)
+    }
+  }
+}
+
+function drawTile(across, down, step){
+  
+  push()
+  translate((across+1)*step, (down+0.5)*step)
+  angleMode(DEGREES)
+  rotate(frameCount+down*10,across*100)
+  rect(10,-10,step*1.5, step)
+  
+  pop()
 }
